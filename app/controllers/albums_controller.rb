e4b27c30fa70f38d307fe8b50a7cbc5259e8ad9d
codecademy <ccuser@codecademy.com>
2016-07-22 00:22:15 -0400

    Initial commit

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,17 @@
+class AlbumsController < ApplicationController
+
+  before_action :require_user, only: [:index, :show]
+  
+  def index
+    @albums = Album.all
+  end 
+
+  def show
+    @album = Album.find(params[:id])
+    @photos = @album.photos
+  end
+  
+  
+
+end
+
